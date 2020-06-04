f8e12eb25df8bd22fb3d468472456b1e8c9a41cd
Gautier Miquet <xisabla.dev@gmail.com>
2020-06-04 11:56:43 +0200

    add some examples for mongodb and API fetch

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,74 @@
-console.log('Hello World')
+import path from 'path'
+import express from 'express'
+import mongoose from 'mongoose'
+import fetch from 'node-fetch'
+import { config } from 'dotenv'
+
+config()
+
+const mongoUrl = process.env.MONGO_URL
+const mongoUser = process.env.MONGO_USER
+const mongoPass = process.env.MONGO_PASS
+const mongoDb = process.env.MONGO_DB
+
+/*
+
+API FETCH EXAMPLE
+
+fetch('https://api.github.com/users/Xisabla/repos')
+.then((res) => res.json())
+.then((data) => console.log(data))
+.catch((err) => console.log(err))
+
+*/
+
+
+/*
+
+SERVER CONFIGURATION
+
+const app = express()
+
+const publicPath: string = path.join(__dirname, '../', 'public')
+
+app.use(express.static(publicPath))
+
+
+DATABASE CONNECTION
+
+mongoose.connect(mongoUrl, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    user: mongoUser,
+    pass: mongoPass,
+    dbName: mongoDb
+})
+.then(() => {
+
+    SERVER START
+
+    app.listen(3000, () => {
+        console.log('Server running')
+        console.log('http://localhost:3000')
+    })
+
+    TRY TO ADD A NEW USER TO "accounts" COLLECTION
+
+    const userSchema = new mongoose.Schema({
+        name: { type: String, unique: true, required: true }
+    }, { collection: 'accounts' })
+
+    const User = mongoose.model('User', userSchema)
+
+    const Uranium32 = new User({ name: 'Uranium32'})
+
+    Uranium32.save()
+        .then(() => { console.log('saved')})
+        .catch((err) => { console.log(err.code)})
+
+})
+.catch(() => {
+    console.log('Unable to connect to the database. Application not running')
+})
+
+*/
