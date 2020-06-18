de20815852c83c6164fb5ab2d8555d86485cada5
Gautier Miquet <xisabla.dev@gmail.com>
2020-06-18 11:22:01 +0200

    getStorage with empty name now deserve root storage

--- server/src/core/Application.ts
+++ server/src/core/Application.ts
@@ -153,7 +153,7 @@ export default class Application {
      * @param name Name of the sub-storage
      * @returns The path to the sub-storage directory
      */
-    public getStorage(name: string) {
+    public getStorage(name = '') {
         const dir = path.join(this._options.storage, name)
 
         if (!fs.existsSync(dir)) {
